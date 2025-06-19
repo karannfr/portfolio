b4cd9e9bbcb9ff685c275e6ea1aa067d7968d588
Karan Dugar <krishdugar04@gmail.com>
2025-06-20 05:13:40 +0530

    added loader dismiss

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -9,6 +9,10 @@ const RootLayout = () => {
   const [date,setDate] = useState<Date | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    document.addEventListener("keydown",() => {
+      if(isLoading)
+        setIsLoading(false);
+    })
     setDate(new Date());
     setInterval(() => {
       setDate(new Date());
@@ -21,7 +25,7 @@ const RootLayout = () => {
   },[])
   return (
     isLoading ?
-    <div className="flex min-h-screen justify-center items-center">
+    <div className="flex min-h-screen justify-center items-center" onClick={() => {setIsLoading(prev => !prev)}}>
       <DotLottieReact
         src="https://lottie.host/644d45db-d6f9-4d3a-bc67-4bd35117132f/njF5KyG19W.lottie"
         autoplay
