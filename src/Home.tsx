0f2f9666422593437e22c36875b3cef34376056b
Karan Dugar <krishdugar04@gmail.com>
2025-06-22 03:27:05 +0530

    added project

--- src/Home.tsx
+++ src/Home.tsx
@@ -63,7 +63,7 @@ const Home = () => {
               <p className='text-sm sm:text-base md:text-lg text-zinc-300 leading-relaxed'>Your Personalized NPTEL Quiz Companion</p>
               <a href="https://quiztelify.karnx.dev" target='_blank' className='group flex flex-row space-between items-center gap-4 px-7 sm:px-12 sm:text-xl py-4 bg-white text-black rounded-md text-sm font-semibold  transition delay-150 duration-300 ease-in-out hover:bg-purple-400 hover:cursor-[url(/hover.svg),_pointer]'>Vew Project <TfiArrowTopRight className='transition-transform duration-300 ease-in-out group-hover:rotate-45'/></a>
             </motion.div>
-            <motion.div className='flex flex-col items-start gap-4 cursor-[url(/QuiztelifyCursor.svg),_pointer] lg:w-80 xl:w-96 2xl:w-[625px]'
+            <motion.div className='flex flex-col items-start gap-4 cursor-[url(/logo_ffcs.svg),_pointer] lg:w-80 xl:w-96 2xl:w-[625px]'
                initial={{ opacity: 0, scale: 0.5 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 transition={{
@@ -72,10 +72,10 @@ const Home = () => {
                     ease: [0, 0.71, 0.2, 1.01],
                 }}
               >
-              <img className="rounded-md"src="/Quiztelify.png" alt="" />
-              <h1 className='text-xl sm:text-xl md:text-2xl font-semibold'>Quiztelify</h1>
-              <p className='text-sm sm:text-base md:text-lg text-zinc-300 leading-relaxed'>Your Personalized NPTEL Quiz Companion</p>
-              <a href="https://quiztelify.karnx.dev" target='_blank' className='group flex flex-row space-between items-center gap-4 px-7 sm:px-12 sm:text-xl py-4 bg-white text-black rounded-md text-sm font-semibold  transition delay-150 duration-300 ease-in-out hover:bg-purple-400 hover:cursor-[url(/hover.svg),_pointer]'>Vew Project <TfiArrowTopRight className='transition-transform duration-300 ease-in-out group-hover:rotate-45'/></a>
+              <img className="rounded-md"src="/FFCS.png" alt="" />
+              <h1 className='text-xl sm:text-xl md:text-2xl font-semibold'>FFCS-Inator</h1>
+              <p className='text-sm sm:text-base md:text-lg text-zinc-300 leading-relaxed'>Create Your Ideal Timetable</p>
+              <a href="https://ffcs.codechefvit.com" target='_blank' className='group flex flex-row space-between items-center gap-4 px-7 sm:px-12 sm:text-xl py-4 bg-white text-black rounded-md text-sm font-semibold  transition delay-150 duration-300 ease-in-out hover:bg-purple-400 hover:cursor-[url(/hover.svg),_pointer]'>Vew Project <TfiArrowTopRight className='transition-transform duration-300 ease-in-out group-hover:rotate-45'/></a>
             </motion.div>
             <motion.div className='flex flex-col items-start gap-4 cursor-[url(/codechef.svg),_pointer] lg:w-80 xl:w-96 2xl:w-[625px]'
                initial={{ opacity: 0, scale: 0.5 }}
